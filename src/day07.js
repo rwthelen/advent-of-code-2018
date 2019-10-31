506cb91699d0670ddea3f1023d2db9baeb265216
Ross Thelen <rthelen@apple.com>
2019-10-31 08:09:58 -0700

    day07

--- src/day07.js
+++ src/day07.js
@@ -1,84 +1,137 @@
-class Solution {
+const assert = require("assert");
+const util = require("util");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+
+const TEST_INPUT = `Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.`;
+const TEST_PART_1_WORKERS = 1;
+const TEST_PART_1_BASE_TASK_TIME = 0;
+const TEST_PART_1_SOLUTION = "CABDFE";
+
+const TEST_PART_2_WORKERS = 2;
+const TEST_PART_2_BASE_TASK_TIME = 0;
+const TEST_PART_2_SOLUTION = 15;
+
+class Worker {
+  constructor(id) {
+    this.id = "w" + (id + 1);
+    this.assignment = null;
+    this.timeDone = Infinity;
+  }
+
+  completeAssignment() {
+    this.assignment.complete();
+    this.assignment = null;
+    this.doneAt = Infinity;
+  }
+
+  setAssignment(step, timeDone) {
+    this.assignment = step;
+    this.assignment.assigned = true;
+    this.timeDone = timeDone;
+  }
+
+  getAssignment() {
+    if (this.assignment) {
+      return this.assignment;
+    } else {
+      return { name: "-" };
+    }
+  }
+
+  isEveryDepCompleted() {}
+}
+
+class Step {
+  constructor(name, deps, job) {
+    this.name = name;
+    this.deps = deps;
+    this.completed = false;
+    this.assigned = false;
+    this.job = job;
+  }
+
+  complete() {
+    this.completed = true;
+  }
+
+  isEveryDepCompleted() {
+    return this.deps.every(dep => this.job.completedSteps.includes(dep));
+  }
+
+  getDependencies() {
+    const deps = this.job.input
+      .split("\n")
+      .filter(line => line.substring(36, 37) === this.name)
+      .map(line => line.substring(5, 6));
+    return deps;
+  }
+}
+
+class Job {
   constructor(input) {
     this.steps = this.getSteps(input);
     this.completedSteps = [];
-    this.workLog = [];
-  }
-
-  getAnswer() {
-    this.completeAllSteps();
-    return this.completedSteps.join("");
-  }
-
-  //TODO: Refactor with completeAllSteps
-  getTotalTime(workers, baseTaskTime) {
-    var curSecond = 0;
-    var workers = Array.from({ length: workers }, (_, i) => ({
-      id: i + 1,
-      busy: false,
-      doneAt: Infinity
-    }));
-    while (!this.steps.every(step => step.completed)) {
-      for (const worker of workers) {
-        // on each tick, need to iterate through all workers and update their work
-        if (worker.busy) {
-          // if they're already working, need to determine if they're done
-          // if they're done, need to mark the step complete
-          // the worker will get assigned a new task on the next tick
-          if (worker.doneAt === curSecond) {
-            this.completeStep(next);
-            this.completedSteps.push(next.name);
-          }
-        } else {
-          // See if there is available work to assign
-          // if there is available work, assign it to the worker
-          const next = this.findNextStep();
-          if (next) {
-            const stepDuration = baseTaskTime + (next.name.charCodeAt() - 64);
-            worker.doneAt = curSecond + stepDuration;
-            worker.busy = true;
-            next.assigned = true;
-          }
-        }
-      }
-
-      curSecond++;
-    }
-  }
-
-  reset() {
-    for (const step of this.steps) {
-      step.completed = false;
-    }
-    this.completeSteps = [];
+    this.timeLog = [];
   }
 
   getSteps(input) {
-    var steps = input
-      .split("\n")
-      .map(l => [l.substring(36, 37), l.substring(5, 6)])
-      .reduce((prev, curr) => prev.concat(curr));
-    steps = Array.from(new Set(steps));
-    return steps.map(step => {
-      return {
-        name: step,
-        deps: this.getStepDependencies(step, input),
-        completed: false,
-        assigned: false
-      };
-    });
-  }
-
-  completeAllSteps() {
-    while (this.completedSteps.length < this.steps.length) {
-      const next = this.findNextStep();
-      this.completeStep(next);
-      this.completedSteps.push(next.name);
+    function reducer(accumulator, steps) {
+      for (const step of steps) {
+        if (!accumulator[step]) {
+          accumulator[step] = [];
+        }
+      }
+      const [dep, step] = steps;
+      accumulator[step].push(dep);
+      return accumulator;
     }
+
+    const stepsMap = input
+      .split("\n")
+      .map(l => [l.substring(5, 6), l.substring(36, 37)])
+      .reduce(reducer, {});
+
+    const steps = Object.keys(stepsMap).map(
+      key => new Step(key, stepsMap[key], this)
+    );
+    return steps;
   }
 
-  completeStep(step) {
-    step.completed = true;
+  execute(numWorkers, baseTaskTime) {
+    let curTime = 0;
+    const workers = Array.from({ length: numWorkers }, (_, i) => new Worker(i));
+    while (!this.steps.every(step => step.completed)) {
+      let curState = {};
+      for (const worker of workers) {
+        if (worker.assignment) {
+          if (worker.timeDone === curTime) {
+            this.completedSteps.push(worker.assignment.name);
+            worker.completeAssignment();
+          }
+        }
+
+        if (worker.assignment === null) {
+          const next = this.findNextStep();
+          if (next) {
+            const stepDuration = baseTaskTime + (next.name.charCodeAt() - 64);
+            const timeDone = curTime + stepDuration;
+            worker.setAssignment(next, timeDone);
+          }
+        }
+        curState[worker.id] = worker.getAssignment();
+      }
+      curState["Second"] = curTime;
+      curState["Done"] = this.completedSteps.join("");
+      this.timeLog.push(curState);
+      curTime++;
+    }
   }
 
   findNextStep() {
@@ -94,70 +147,62 @@ class Solution {
     }
     const next = this.steps
       .filter(step => !step.completed && !step.assigned)
-      .filter(step => this.isEveryDepCompleted(step))
+      .filter(step => step.isEveryDepCompleted())
       .sort(sortFn)[0];
     return next;
   }
+}
 
-  isEveryDepCompleted(step) {
-    return step.deps.every(dep => this.completedSteps.includes(dep));
+class Main {
+  constructor(input, numWorkers, baseTaskTime) {
+    this.input = input;
+    this.numWorkers = numWorkers;
+    this.baseTaskTime = baseTaskTime;
   }
 
-  getStepDependencies(step, input) {
-    const deps = input
-      .split("\n")
-      .filter(line => line.substring(36, 37) === step)
-      .map(line => line.substring(5, 6));
-    return deps;
+  main() {
+    const job = new Job(this.input);
+    job.execute(this.numWorkers, this.baseTaskTime);
+    return job.completedSteps.join("");
   }
 }
 
+function testPart1() {
+  const solution = new Main(
+    TEST_INPUT,
+    TEST_PART_1_WORKERS,
+    TEST_PART_1_BASE_TASK_TIME
+  ).main();
+  assert.equal(solution, TEST_PART_1_SOLUTION);
+  console.log(`The Part One test passed, the solution is ${solution}`);
+}
+
+async function testPart2() {
+  const job = new Job(TEST_INPUT);
+  job.execute(TEST_PART_2_WORKERS, TEST_PART_2_BASE_TASK_TIME);
+  const solution = job.timeLog[job.timeLog.length - 1].Second;
+  assert.equal(solution, TEST_PART_2_SOLUTION);
+  console.log(`The Part Two test passed, the solution is ${solution}`);
+}
+
+async function solvePart2() {
+  const input = await readFile(__dirname + "/../input/day07.txt", "utf8");
+  const job = new Job(input);
+  job.execute(5, 60);
+  const solution = job.timeLog[job.timeLog.length - 1].Second;
+  // assert.equal(solution, TEST_PART_2_SOLUTION);
+  console.log(`Part Two Success! The solution is ${solution}`);
+}
+
+async function solvePart1() {
+  const input = await readFile(__dirname + "/../input/day07.txt", "utf8");
+  const solution = new Main(input, 1, 0).main();
+  console.log(`Part One Success! The solution is: ${solution}`);
+}
+
 if (require.main === module) {
-  const util = require("util");
-  const fs = require("fs");
-  const readFile = util.promisify(fs.readFile);
-
-  async function tests() {
-    const input = `Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.`;
-    const theAnswer = "CABDFE";
-    const solution = new Solution(input);
-    const answer = solution.getAnswer();
-    if (answer === theAnswer) {
-      console.log("Success!");
-    } else {
-      throw "YOU FAIL";
-    }
-    solution.reset();
-    const totalTime = solution.getTotalTime(2, 0);
-    console.log(totalTime);
-  }
-
-  async function test2() {
-    const input = `Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.`;
-    const theAnswer = 15;
-    const solution = new Solution(input);
-    const totalTime = solution.getTotalTime(2, 0);
-    console.log(totalTime);
-  }
-
-  async function main() {
-    const input = await readFile(__dirname + "/../input/day07.txt", "utf8");
-    const answer = new Solution(input).getAnswer();
-    console.log(answer);
-  }
-
-  test2();
-  // main();
+  testPart1();
+  solvePart1();
+  testPart2();
+  solvePart2();
 }
