05b0a57a53bba3942866771c94d3c0deac74f628
Ross Thelen <rossthelen@rosss-mac-mini.lan>
2019-11-14 13:54:41 -0800

    day01 refactor

--- src/day01.js
+++ src/day01.js
@@ -1,54 +1,41 @@
+const util = require("util");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+
 class Solution {
-    constructor(input){
-        this.frequency = 0;
-        this.input = input.map((frequencyChange) => parseInt(frequencyChange));
-        this.frequencyLog = [this.frequency];
-        this.repeatedFrequency = null;
-    }
+  constructor(input) {
+    this.input = input.split("\n").map(n => parseInt(n));
+  }
 
-    calculateFrequency() {
-        const reducer = (frequency, frequencyChange) => frequency + frequencyChange;
-        const finalFrequency = this.input.reduce(reducer, this.frequency);
-        console.log(`The solution for Day 1, Part 1 is: ${finalFrequency}`);
-    }
+  calculateFrequency() {
+    const finalFrequency = this.input.reduce((a, b) => a + b, 0);
+    console.log(`The solution for Day 1, Part 1 is: ${finalFrequency}`);
+  }
 
-    calibrateFrequency() {
-        const reducer = (frequency, frequencyChange) => {
-            const newFrequency = frequency + frequencyChange;
-            if (this.frequencyLog.includes(newFrequency) && this.repeatedFrequency == null) {
-                this.repeatedFrequency = newFrequency;
-            }
-            this.frequencyLog.push(newFrequency);
-            return newFrequency;
+  calibrateFrequency() {
+    let frequency = 0;
+    let duplicate = null;
+    let seen = new Set();
+    while (!duplicate) {
+      for (let f of this.input) {
+        if (seen.has(frequency)) {
+          duplicate = true;
+          break;
+        } else {
+          seen.add(frequency);
+          frequency += f;
         }
-
-        while (this.repeatedFrequency == null) {
-            this.frequency = this.input.reduce(reducer, this.frequency);
-        }
-
-        console.log(`The solution for Day 1, Part 2 is: ${this.repeatedFrequency}`);
+      }
     }
+    console.log(`The solution for Day 1, Part 2 is: ${frequency}`);
+  }
 }
 
-if (require.main === module) {
-    const util = require("util");
-    const fs = require("fs");
-    const readFile = util.promisify(fs.readFile);
-
-    async function getInput(inputPath) {
-        let input = await readFile(inputPath, "utf8")
-            .then((input) => input.split("\n"))
-        return input;
-    }
-    
-    async function main() {
-        const input = await getInput(__dirname + "/../../input/day01.txt");
-        const solution = new Solution(input);
-        solution.calculateFrequency();
-        solution.calibrateFrequency();
-    }
-
-    main();
+async function main() {
+  const input = await readFile(__dirname + "/../input/day01.txt", "utf-8");
+  const solution = new Solution(input);
+  solution.calculateFrequency();
+  solution.calibrateFrequency();
 }
 
-module.exports = Solution;
+main();
