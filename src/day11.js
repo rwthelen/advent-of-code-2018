87c8d855e8136c1af681d6300450651e6b64d187
Ross Thelen <rossthelen@rosss-mac-mini.lan>
2019-11-30 15:33:05 -0800

    day11

--- src/day11.js
+++ src/day11.js
@@ -1,118 +1,85 @@
 const assert = require("assert");
 
-const minReducer = (curMin, current) => (current < curMin ? current : curMin);
-const maxReducer = (curMax, current) => (current > curMax ? current : curMax);
-
-class Grid {
-  constructor(serialNumber, size, startingCoord = [1, 1]) {
-    this.serialNumber = serialNumber;
-    this.size = size;
-    this.startingCoord = startingCoord;
-    this.setBounds();
-    this.cells = this.generateCells();
-  }
-
-  setBounds() {
-    this.xMin = this.startingCoord[0];
-    this.xMax = this.xMin + (this.size - 1);
-    this.yMin = this.startingCoord[1];
-    this.yMax = this.yMin + (this.size - 1);
-  }
-
-  generateCells() {
-    let points = [];
-    for (let y = this.yMin; y <= this.yMax; y++) {
-      for (let x = this.xMin; x <= this.xMax; x++) {
-        points.push(new Cell(x, y, this));
-      }
+function calcGrid(size = 300, sn = 18, anySize = false) {
+  // create the summed-area table
+  let grid = [];
+  for (let y = 0; y < size; y++) {
+    grid[y] = [];
+    for (let x = 0; x < size; x++) {
+      const ll = (grid[y] || [])[x - 1] || 0;
+      const ur = (grid[y - 1] || [])[x] || 0;
+      const ul = (grid[y - 1] || [])[x - 1] || 0;
+      let summedArea = calcPowerLevel(x + 1, y + 1, sn) + ll + ur - ul;
+      grid[y][x] = summedArea;
     }
-    return points;
   }
-
-  getTotalPowerLevel() {
-    return this.cells
-      .map(c => c.calcPowerLevel())
-      .reduce((accumulator, current) => accumulator + current);
-  }
-
-  calcBestSquare() {
-    let powerLevels = [];
-    for (let y = this.yMin; y <= this.yMax; y++) {
-      for (let x = this.xMin; x <= this.xMax; x++) {
-        if (x <= this.xMax - 2) {
-          let grid = new Grid(this.serialNumber, 3, [x, y]);
-          powerLevels.push({ coord: [x, y], value: grid.getTotalPowerLevel() });
+  // create the grid of power levels for each subSize,
+  // using the summed-area table to calculate each one
+  let best = { total: -Infinity };
+  for (let subSize = 1; subSize <= size; subSize++) {
+    if (!anySize && subSize !== 3) {
+      continue;
+    }
+    for (let y = 0; y < size - subSize; y++) {
+      for (let x = 0; x < size - subSize; x++) {
+        const ul = (grid[y - 1] || [])[x - 1] || 0;
+        const ur = (grid[y - 1] || [])[x + (subSize - 1)] || 0;
+        const lr = (grid[y + (subSize - 1)] || [])[x + (subSize - 1)] || 0;
+        const ll = (grid[y + (subSize - 1)] || [])[x - 1] || 0;
+        const total = ul + lr - ur - ll;
+        if (total > best.total) {
+          best = { x: x + 1, y: y + 1, total: total, subSize: subSize };
         }
       }
     }
-    const best = powerLevels.reduce((accumulator, current) => {
-      if (current.value > accumulator.value) {
-        return current;
-      } else {
-        return accumulator;
-      }
-    });
-    return best;
   }
+  return best;
 }
 
-class Cell {
-  constructor(x, y, grid = null) {
-    this.x = x;
-    this.y = y;
-    this.rackId = this.x + 10;
-    this.grid = grid;
-  }
-
-  calcPowerLevel() {
-    let powerLevel = this.rackId * this.y;
-    powerLevel += this.grid.serialNumber;
-    powerLevel *= this.rackId;
-    powerLevel = Math.floor((powerLevel / 100) % 10);
-    powerLevel -= 5;
-    return powerLevel;
-  }
+function calcPowerLevel(x, y, sn) {
+  let rackId = x + 10;
+  return Math.floor((((rackId * y + sn) * rackId) / 100) % 10) - 5;
 }
 
 function testFuelCells() {
-  const cell1 = new Cell(122, 79);
-  cell1.grid = { serialNumber: 57 };
-  assert.equal(cell1.calcPowerLevel(), -5);
-  const cell2 = new Cell(217, 196);
-  cell2.grid = { serialNumber: 39 };
-  assert.equal(cell2.calcPowerLevel(), 0);
-  const cell3 = new Cell(101, 153);
-  cell3.grid = { serialNumber: 71 };
-  assert.equal(cell3.calcPowerLevel(), 4);
-}
-
-function part1(serialNumber, coords) {
-  const SERIAL_NUMBER = serialNumber;
-  const COORDS = coords;
-  const SIZE = 300;
-  const grid = new Grid(SERIAL_NUMBER, SIZE);
-  if (coords) {
-    assert.equal(grid.calcBestSquare().coord.join(", "), COORDS.join(", "));
-  } else {
-    console.log(grid.calcBestSquare().coord);
-  }
+  const test1 = calcPowerLevel(122, 79, 57);
+  const test2 = calcPowerLevel(217, 196, 39);
+  const test3 = calcPowerLevel(101, 153, 71);
+  assert.equal(test1, -5);
+  assert.equal(test2, 0);
+  assert.equal(test3, 4);
 }
 
 function testPart1() {
-  const SERIAL_NUMBER = 18;
-  const COORDS = [33, 45];
-  part1(SERIAL_NUMBER, COORDS);
+  const solution = calcGrid();
+  const coords = [solution.x, solution.y].join(",");
+  assert.equal(coords, "33,45");
 }
 
 function testPart1_2() {
-  const SERIAL_NUMBER = 42;
-  const COORDS = [21, 61];
-  part1(SERIAL_NUMBER, COORDS);
+  const solution = calcGrid(300, 42);
+  const coords = [solution.x, solution.y].join(",");
+  assert.equal(coords, "21,61");
 }
 
 function solvePart1() {
-  const SERIAL_NUMBER = 7672;
-  part1(SERIAL_NUMBER);
+  const solution = calcGrid(300, 7672);
+  const coords = [solution.x, solution.y].join(",");
+  assert.equal(coords, "22,18");
+  console.log(coords);
+}
+
+function testPart2() {
+  const solution = calcGrid(300, 18, true);
+  const coords = [solution.x, solution.y, solution.subSize].join(",");
+  assert.equal(coords, "90,269,16");
+}
+
+function solvePart2() {
+  const solution = calcGrid(300, 7672, true);
+  const coords = [solution.x, solution.y, solution.subSize].join(",");
+  console.log(coords);
+  assert.equal(coords, "234,197,14");
 }
 
 if (require.main === module) {
@@ -120,4 +87,6 @@ if (require.main === module) {
   testPart1();
   testPart1_2();
   solvePart1();
+  testPart2();
+  solvePart2();
 }
